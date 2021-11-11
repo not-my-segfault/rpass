e1b9edc7a5efe05e3f59e638c0a4db7f9e835714
Michal Stopyra <michal@tar.black>
2021-11-11 23:09:26 +0000

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use text_io::read;
+use rand::distributions::Alphanumeric;
 use rand::Rng;
 use std::env;
-use rand::distributions::Alphanumeric;
+use text_io::read;
 
 fn main() {
     let dir = format!("{}/.local/passman", env::var("HOME").unwrap());
@@ -16,15 +16,17 @@ fn main() {
     if !std::path::Path::new(&prefix_path).exists() {
         let mut rng = rand::thread_rng();
         println!("Noting first run. Generating new prefix.");
-        let prefix: String = (0..7).map(|_| {
-            let x = rng.sample(Alphanumeric);
-            x as char
-        }).collect();
+        let prefix: String = (0..7)
+            .map(|_| {
+                let x = rng.sample(Alphanumeric);
+                x as char
+            })
+            .collect();
         std::fs::write(&prefix_path, prefix).unwrap();
     };
 
     if !std::path::Path::new(&secret_path).exists() {
-        println!("Noting first run. Please enter a memorable secret."); 
+        println!("Noting first run. Please enter a memorable secret.");
         let secret: String = read!("{}\n");
         std::fs::write(&secret_path, secret).unwrap();
     };
@@ -32,10 +34,12 @@ fn main() {
     if !std::path::Path::new(&cipher_path).exists() {
         let mut rng = rand::thread_rng();
         println!("Noting first run. Generating new cipher.");
-        let cipher: String = (0..26).map(|_| {
-            let x = rng.sample(Alphanumeric);
-            x as char
-        }).collect();
+        let cipher: String = (0..26)
+            .map(|_| {
+                let x = rng.sample(Alphanumeric);
+                x as char
+            })
+            .collect();
         std::fs::write(&cipher_path, cipher).unwrap();
     };
 
@@ -46,14 +50,21 @@ fn main() {
     println!("App Name: ");
     let app: String = read!();
 
-    let app_ciphered = cipher.chars().zip(app.chars()).map(|(c, a)| {
-        let c = c as u8;
-        let a = a as u8;
-        let c = c ^ a;
-        c as char
-    }).collect::<String>();
- 
+    let app_ciphered = cipher
+        .chars()
+        .zip(app.chars())
+        .map(|(c, a)| {
+            let c = c as u8;
+            let a = a as u8;
+            let c = c ^ a;
+            c as char
+        })
+        .collect::<String>();
+
     let pass = format!("{}{}{}", prefix, secret, app_ciphered);
 
-    println!("Your password for {} is: {} [ {} + {} + {} ]", app, pass, prefix, secret, app_ciphered);
+    println!(
+        "Your password for {} is: {} [ {} + {} + {} ]",
+        app, pass, prefix, secret, app_ciphered
+    );
 }
