db190d5680d6e3438336edb9d3741080b7b13abe
Michal Stopyra <michal@tar.black>
2021-11-11 20:01:05 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use text_io::read;
+use rand::Rng;
+use std::env;
+use rand::distributions::Alphanumeric;
+
+fn main() {
+    let dir = format!("{}/.local/passman", env::var("HOME").unwrap());
+    let prefix_path = format!("{}/{}", dir, "prefix");
+    let secret_path = format!("{}/{}", dir, "secret");
+    let cipher_path = format!("{}/{}", dir, "cipher");
+
+    if !std::path::Path::new(&dir).exists() {
+        std::fs::create_dir_all(&dir).unwrap();
+    };
+
+    if !std::path::Path::new(&prefix_path).exists() {
+        let mut rng = rand::thread_rng();
+        println!("Noting first run. Generating new prefix.");
+        let prefix: String = (0..7).map(|_| {
+            let x = rng.sample(Alphanumeric);
+            x as char
+        }).collect();
+        std::fs::write(&prefix_path, prefix).unwrap();
+    };
+
+    if !std::path::Path::new(&secret_path).exists() {
+        println!("Noting first run. Please enter a memorable secret."); 
+        let secret: String = read!("{}\n");
+        std::fs::write(&secret_path, secret).unwrap();
+    };
+
+    if !std::path::Path::new(&cipher_path).exists() {
+        let mut rng = rand::thread_rng();
+        println!("Noting first run. Generating new cipher.");
+        let cipher: String = (0..26).map(|_| {
+            let x = rng.sample(Alphanumeric);
+            x as char
+        }).collect();
+        std::fs::write(&cipher_path, cipher).unwrap();
+    };
+
+    let prefix = std::fs::read_to_string(&prefix_path).unwrap();
+    let secret = std::fs::read_to_string(&secret_path).unwrap();
+    let cipher = std::fs::read_to_string(&cipher_path).unwrap();
+
+    println!("App Name: ");
+    let app: String = read!();
+
+    let app_ciphered = cipher.chars().zip(app.chars()).map(|(c, a)| {
+        let c = c as u8;
+        let a = a as u8;
+        let c = c ^ a;
+        c as char
+    }).collect::<String>();
+ 
+    let pass = format!("{}{}{}", prefix, secret, app_ciphered);
+
+    println!("Your password for {} is: {} [ {} + {} + {} ]", app, pass, prefix, secret, app_ciphered);
+}
