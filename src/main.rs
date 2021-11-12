d28c82c0de9a0d2852421eb92f32011d0958c497
Michal Stopyra <michal@tar.black>
2021-11-12 11:41:47 +0000

    added comments

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,16 @@ fn main() {
     let secret_path = format!("{}/{}", dir, "secret");
     let cipher_path = format!("{}/{}", dir, "cipher");
 
+    // making sure our directory exists
     if !std::path::Path::new(&dir).exists() {
         std::fs::create_dir_all(&dir).unwrap();
     };
 
+    // prefix generation (if not present)
     if !std::path::Path::new(&prefix_path).exists() {
         let mut rng = rand::thread_rng();
         println!("Noting first run. Generating new prefix.");
+        // random alphanumeric string of length 7
         let prefix: String = (0..7)
             .map(|_| {
                 let x = rng.sample(Alphanumeric);
@@ -23,17 +26,25 @@ fn main() {
             })
             .collect();
         std::fs::write(&prefix_path, prefix).unwrap();
+        // making sure only the owner can only read the file
+        std::fs::set_permissions(&prefix_path, std::fs::Permissions::from_mode(0o400)).unwrap();
     };
 
+    // collecting secret (if not present)
     if !std::path::Path::new(&secret_path).exists() {
         println!("Noting first run. Please enter a memorable secret.");
         let secret: String = read!("{}\n");
         std::fs::write(&secret_path, secret).unwrap();
+        // making sure only the owner can only read the file
+        std::fs::set_permissions(&secret_path, std::fs::Permissions::from_mode(0o400)).unwrap();
     };
 
+    // cipher generation (if not present)
     if !std::path::Path::new(&cipher_path).exists() {
+        // initialising rng
         let mut rng = rand::thread_rng();
         println!("Noting first run. Generating new cipher.");
+        // random alphanumeric string of length 26
         let cipher: String = (0..26)
             .map(|_| {
                 let x = rng.sample(Alphanumeric);
@@ -41,15 +52,20 @@ fn main() {
             })
             .collect();
         std::fs::write(&cipher_path, cipher).unwrap();
+        // making sure only the owner can only read the file
+        std::fs::set_permissions(&cipher_path, std::fs::Permissions::from_mode(0o400)).unwrap();
     };
 
+    // read the values from the files
     let prefix = std::fs::read_to_string(&prefix_path).unwrap();
     let secret = std::fs::read_to_string(&secret_path).unwrap();
     let cipher = std::fs::read_to_string(&cipher_path).unwrap();
 
+    // collect the app name from the user
     println!("App Name: ");
     let app: String = read!();
 
+    // mapp the app name against the cipher
     let app_ciphered: String = app
         .chars()
         .map(|x| {
@@ -64,9 +80,11 @@ fn main() {
             y
         })
         .collect();
-
+    
+    // format the final password
     let pass = format!("{}{}{}", prefix, secret, app_ciphered);
 
+    // print to the user
     println!(
         "Your password for {} is: {} [ {} + {} + {} ]",
         app, pass, prefix, secret, app_ciphered
