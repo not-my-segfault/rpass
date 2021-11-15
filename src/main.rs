3f20d718ca9408b9cc18d36af2b0199e3f04e04c
michal <michal@tar.black>
2021-11-15 18:03:17 +0000

    on cipher/prefix generation, tars up files and stores them at ~/.rpass_backup.tar

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,24 @@ use util::{help, tut};
 fn main() {
     let dir = format!("{}/{}", dirs::home_dir().unwrap().to_str().unwrap(), ".rpass");
     let args: Vec<String> = env::args().collect();
+    // if no arguments are passed, passes it to the passgen() function to prompt for app name
     if args.len() <= 1 {
         let arg: String = "".to_string();
         passgen(arg);
         exit(0);
     }
+    // shows the tutorial if the user passes the --tut flag
     if args[1] == "--tut" {
         tut();
         exit(0);
+    // removes the .rpass directory if the user passes the --clean flag
     } else if args[1] == "--clean" {
         let _ = std::fs::remove_dir_all(dir);
+    // displays the help output if the user passes the --help flag
     } else if args[1] == "--help" {
         help();
         exit(0);
+    // catches any other arguments and passes them to the passgen() function
     } else {
         let arg: String = args[1].clone();
         passgen(arg);
@@ -30,7 +35,11 @@ fn main() {
 }
 
 fn passgen(arg: String) {
-    const CHARSET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}\\|;:'\",<.>/?`~";
+    let nchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let schars = "!@#$%^&*()-_=+[]{}\\|;:'\",<.>/?`~";
+
+    // adjust the .repeat(x) values to change the ration of normal characters to special characters
+    let charset = format!("{}{}", nchars.repeat(2), schars.repeat(1));
 
     // this variable is set to the home directory of the user on windows
     let dir = format!("{}/{}", dirs::home_dir().unwrap().to_str().unwrap(), ".rpass");
@@ -46,7 +55,7 @@ fn passgen(arg: String) {
     // prefix generation (if not present)
     if !std::path::Path::new(&prefix_path).exists() {
         // random alphanumeric string of length 7
-        let prefix: String = generate(7, &CHARSET);
+        let prefix: String = generate(7, &charset);
         std::fs::write(&prefix_path, prefix).unwrap();
         // making sure only the owner can only read the file
         let mut permissions = std::fs::metadata(&prefix_path).unwrap().permissions();
@@ -56,11 +65,22 @@ fn passgen(arg: String) {
     // cipher generation (if not present)
     if !std::path::Path::new(&cipher_path).exists() {
         // random alphanumeric string of length 26
-        let cipher: String = generate(512, &CHARSET);
+        let cipher: String = generate(512, &charset);
         std::fs::write(&cipher_path, cipher).unwrap();
         // making sure only the owner can only read the file
         let mut permissions = std::fs::metadata(&cipher_path).unwrap().permissions();
         permissions.set_readonly(true);
+    };
+
+    // back up the rpass directory
+    let backup_path = format!("{}/{}", dirs::home_dir().unwrap().to_str().unwrap(), ".rpass_backup.tar");
+    if !std::path::Path::new(&backup_path).exists() {
+        let _ = std::process::Command::new("tar")
+            .arg("-cf")
+            .arg(&backup_path)
+            .arg(&dir)
+            .output()
+            .expect("Something went wrong creating rpass backup. Check home directory permissions.");
     };
 
     // read the values from the files
